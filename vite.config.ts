ba54aee1917559c99e4d0c841c46909184c8c827
Glenda Lim <freyjaglenda@gmail.com>
2025-09-02 20:46:31 +0800

    Resolve merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -14,12 +13,3 @@ export default defineConfig({
     },
   },
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> origin/main
